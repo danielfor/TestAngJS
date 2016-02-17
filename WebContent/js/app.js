5631354b530f327dc43817ee633d3498a3aa42b4
Daniel Ochoa <Daniel_X_Ochoa@gap.com>
2016-02-17 11:52:46 -0500

    Daniel: tabs added, styles not working.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -4,16 +4,30 @@
 		this.products = gems;
 	});
 	var gems = [ {
-		name : 'Dodecahedron',
+		name : 'Dodecahedron Gem',
 		price : 2.95,
-		description : ' . . . ',
+		description : 'Nice Dodecahedron Gem',
 		canPurchase : true,
 		soldOut : false,
+		images : [ {
+			full : 'img/dode-full.jpg',
+			thumb : 'img/dode.jpg'
+		}, {
+			full : 'img/dode2-full.jpg',
+			thumb : 'img/dode2.jpg'
+		} ]
 	}, {
 		name : 'Pentagonal Gem',
 		price : 9.95,
-		description : ' . . . ',
+		description : 'Nice Pentagonal Gem',
 		canPurchase : true,
 		soldOut : false,
+		images : [ {
+			full : 'img/penta-full.jpg',
+			thumb : 'img/penta.jpg'
+		}, {
+			full : 'img/penta2-full.jpg',
+			thumb : 'img/penta2.jpg'
+		} ]
 	} ]
 })();
